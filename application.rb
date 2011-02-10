d8a5f5eef7ce4a12619f37693027cd2a30844de1
jgv <jvingiano@gmail.com>
2011-02-09 23:08:54 -0500

    adding public and views directories

--- application.rb
+++ application.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'open-uri'
 require 'nokogiri'
-
+require 'erb'
+require 'sass'
 
 get '/' do
   @doc = Nokogiri::XML(open('http://mta.info/status/serviceStatus.txt'))  
   @names = @doc.xpath('//status')
-  @names[7]
+  @status = @names[7]
+  erb :index
 end
