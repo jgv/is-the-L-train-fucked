f5f2355ebebee7f4c6b5a038eea46bd5fced70cd
jgv <jvingiano@gmail.com>
2011-05-31 15:53:22 -0400

    removing logic from the view

--- application.rb
+++ application.rb
@@ -6,5 +6,6 @@ require 'erb'
 get '/' do
   @doc = Nokogiri::XML(open('http://mta.info/status/serviceStatus.txt'))  
   @status = @doc.xpath('//status')[7]
+  @train_status = @status.to_s =~ /GOOD SERVICE/ ? 'nope' : 'yup'
   erb :index
 end
