b1f99bde677bf2bba6138805103342b8ba4843be
Jonathan Vingiano <jvingiano@gmail.com>
2014-04-06 13:11:36 -0400

    handle the mta changing the api endpoint w/o telling anyone

--- application.rb
+++ application.rb
@@ -4,8 +4,16 @@ require 'nokogiri'
 require 'erb'
 
 get '/' do
-  @doc = Nokogiri::XML(open('http://mta.info/status/serviceStatus.txt'))  
-  @status = @doc.xpath('//status')[7]
-  @train_status = @status.to_s =~ /GOOD SERVICE/ ? 'nope' : 'yup'
+  begin
+    @doc = Nokogiri::XML(open('http://web.mta.info/status/serviceStatus.txt'))
+  rescue OpenURI::HTTPError
+    @doc = 404
+  end
+  if @doc != 404
+    @status = @doc.xpath('//status')[7]
+    @train_status = @status.to_s =~ /GOOD SERVICE/ ? 'nope' : 'yup'
+  else
+    @train_status = 'idk'
+  end
   erb :index
 end
